27b56f4a0b30ee128644dce60dc8058b6775b558
Gabriella Piacentino <piacentg@bc.edu>
2023-01-20 17:29:52 -0500

    Initial Commit

---
+++ YouAreAwesome2/YouAreAwesome2App.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesome2App.swift
+//  YouAreAwesome2
+//
+//  Created by Gabriella Piacentino on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesome2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
